9124b58cbefd7ee705f8accf414e60312f08205a
James Henager <jameschenager@gmail.com>
2020-09-18 12:17:44 -0700

    add meaningful fail for delete_tap reducer action

--- src/__tests__/reducers/tap-list-reducer.test.js
+++ src/__tests__/reducers/tap-list-reducer.test.js
@@ -12,6 +12,42 @@ describe('tapListReducer', () => {
       count: 142,
     }
 
+    const currentState = {
+      1: {
+        brewery: 'Breakside',
+        beer: 'Wanderlust',
+        style: 'IPA',
+        price: '6',
+        abv: '5.5',
+        count: 142,
+      },
+      2: {
+        brewery: 'Fort George',
+        beer: 'Vortex',
+        style: 'IPA',
+        price: '6',
+        abv: '7',
+        count: 142,
+      }
+    }
+
+    test('Should successfully delete a tap', () => {
+      action = {
+        type: 'DELETE_TAP',
+        id: 1
+      };
+      expect(tapListReducer(currentState, action)).toEqual({
+        2: {
+          brewery: 'Fort George',
+          beer: 'Vortex',
+          style: 'IPA',
+          price: '6',
+          abv: '7',
+          count: 142,
+        }
+      });
+    });
+
     test('Should successfully add new tap data to masterTapList', () => {
       const { brewery, beer, style, price, abv, count, id } = postData;
       action = {
