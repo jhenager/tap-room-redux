2a52eee3a128d40b9567730fc8af8035348bb1ba
James Henager <jameschenager@gmail.com>
2020-09-18 14:04:32 -0700

    add passing test for if no action is passed into reducer

--- src/__tests__/reducers/tap-list-reducer.test.js
+++ src/__tests__/reducers/tap-list-reducer.test.js
@@ -114,4 +114,7 @@ describe('tapListReducer', () => {
         }
       });
     });
+    test('Should return default state if there is no action type passed into the reducer', () => {
+      expect(tapListReducer({}, { type: null })).toEqual({});
+  });
 })
